45c5def0aff1f1d9819da6a95907270d21451168
tph00300 <tph00100@gmail.com>
2020-05-15 07:59:05 +0900

    nomad vanilaJs learning 2-3 dom with JS

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
-const calculator = {
-  plus: function(a,b)
-  {
-    return a+b;
-  },
-  minus: function(a,b)
-  {
-    return a-b;
-  },
-  multiply: function(a,b)
-  {
-    return a*b;
-  },
-  divide: function(a,b)
-  {
-    return a/b;
-  },
-  pow: function(a,b)
-  {
-    return a**b;
-  }
-}
+const title = document.querySelector("#title");
 
-const a = calculator.plus(4,5);
-const b = calculator.minus(4,3);
-const c = calculator.multiply(4,3);
-const d = calculator.divide(4,3);
-const e = calculator.pow(4,3);
-
-
-console.log(a,b,c,d,e);
+title.innerHTML = "Hi! From JS";
+title.style.color= "red";
+document.title= "I own you now";
